015afa0eec931f21842c5ca0f6c4d2d95bd8c750
ayoub hamdoun <ayoub.hamdoun.g@gmail.com>
2023-02-16 12:24:16 -0800

    1st c program w/ betty coding style

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * this is my 1st discription :)
+ * its a simple function taht return a message
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
