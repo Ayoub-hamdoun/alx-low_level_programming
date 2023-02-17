365bebb7ff882cebac77f2550a3fe09cfb881eb7
ayoub hamdoun <ayoub.hamdoun.g@gmail.com>
2023-02-17 13:11:52 -0800

    betty style fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - entry point
- *
- * Return : 0
- *
+ * main - Entry point
+ * this is my 1st discription :)
+ * its a simple function taht return a message
+ * Return: always 0
  */
 
 int main(void)
