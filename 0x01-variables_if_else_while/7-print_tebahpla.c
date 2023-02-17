1f77044b21403925ba315cb5974a5566730815e2
ayoub hamdoun <ayoub.hamdoun.g@gmail.com>
2023-02-17 13:41:01 -0800

    lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * this is my 1st discription :)
+ * its a simple function taht return a message
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a' ; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
