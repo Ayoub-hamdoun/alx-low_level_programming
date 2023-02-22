2fbbcd709bdae4535ede4b09e8297d00a7085a8f
ayoub hamdoun <ayoub.hamdoun.g@gmail.com>
2023-02-22 15:25:04 -0800

    lower case

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,10 @@ void print_alphabet(void);
  * print_alphabet_x10 - prints alphabet 10 times
  */
 void print_alphabet_x10(void);
+/*
+ * _islower - prints if c is lower case
+ *  @c: character to use
+ * Return: 1 or 0
+ */
+int _islower(int c);
 #endif
