80d545f24798bcd5a4280d2c779cf4e44d74116c
ayoub hamdoun <ayoub.hamdoun.g@gmail.com>
2023-02-22 15:18:32 -0800

    print aplha 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,8 @@ int _putchar(char c);
  * print_alphabet - function to print alphabet
  */
 void print_alphabet(void);
+/*
+ * print_alphabet_x10 - prints alphabet 10 times
+ */
+void print_alphabet_x10(void);
 #endif
