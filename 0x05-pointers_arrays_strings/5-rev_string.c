52b43722ba585c165801bd28ae9bfbd71bddb506
ayoub hamdoun <ayoub.hamdoun.g@gmail.com>
2023-02-27 16:05:57 -0800

    betty fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
- *  * rev_string - reverse a string value
- *   *
- *    * @s: string to print
- *     */
+ * rev_string - reverse a string value
+ *
+ * @s: string to print
+ */
 void rev_string(char *s)
 {
 	int i;
